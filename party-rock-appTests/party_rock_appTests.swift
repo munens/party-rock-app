03edc6eae6a495b03c3232e2aaabb0477ddd5e1c
munens <kaumbuthom@yahoo.com>
2017-04-29 17:18:27 -0700

    Initial Commit

---
+++ party-rock-appTests/party_rock_appTests.swift
@@ -0,0 +1,36 @@
+//
+//  party_rock_appTests.swift
+//  party-rock-appTests
+//
+//  Created by Munene Kaumbutho on 2017-04-29.
+//  Copyright © 2017 Munene Kaumbutho. All rights reserved.
+//
+
+import XCTest
+@testable import party_rock_app
+
+class party_rock_appTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
